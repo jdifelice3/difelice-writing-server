5e9cf2be971ccdce1cdd7a29072862a8e544d1e9
Your Name <johndifelice@protonmail.com>
2025-10-08 22:54:17 -0400

    removed console.logs

--- src/services/works.ts
+++ src/services/works.ts
@@ -3,16 +3,5 @@ import { Works } from '../data/works.js';
 
 export const getWorks = (): Work[] => {
     const results: Work [] = Works;
-    console.log('results', results);
-    // results.push(
-    //     {
-    //         title: 'Traffic Girl Wars',
-    //         description: ' ',
-    //         form: Form.enum.novel,
-    //         wordCount: 60000,
-    //         pageCount: 200,
-    //         genre: Genre.literary
-    //     }
-    // )
     return results;
 } 
