f49ec951c0f0b82f8445242a28d00b6909553b34
Your Name <johndifelice@protonmail.com>
2025-10-10 14:22:30 -0400

    build

--- dist/index.js
+++ dist/index.js
@@ -12,6 +12,10 @@ app.options(/.*/, cors()); // respond to preflight immediately
 console.log("✅ CORS implemented");
 app.use(express.json());
 app.use('/api', pdfRouter);
+// catches 404s and redirects to the homepage.
+app.use((req, res, next) => {
+    res.redirect('/');
+});
 console.log("✅ Middleware loaded");
 //const PORT = process.env.PORT || 3000;
 //console.log(`✅ PORT: ${PORT}`);
